ef1252df3284d8577fb6e8aff5b74bef439c504f
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-07-20 14:10:03 +0200

    'Update'

--- ObjectOrientation/scripts/ObjectOrientation.lua
+++ ObjectOrientation/scripts/ObjectOrientation.lua
@@ -1,17 +1,3 @@
---[[----------------------------------------------------------------------------
-
-  Application Name: ObjectOrientation                                                                                                                         
-                                                                                             
-  Description: 
-  Introduction sample to object oriented programming with Lua and AppStudio
-  
-  This sample gives an overview of using object oriented programming. AppStudio
-  also supports code completion with objects by using colon.  
-  This sample is setting a timer and printing a message to the console when it
-  is set and every time the timer expires. 
-   
-------------------------------------------------------------------------------]]
-
 
 --Start of Global Scope--------------------------------------------------------- 
 
