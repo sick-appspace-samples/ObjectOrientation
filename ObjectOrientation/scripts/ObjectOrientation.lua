5aec173e863171e6c99f6e7f50d369ff7fab8fbe
SICKAppSpace-dev <>
2023-12-20 23:56:13 +0000

    Update

--- ObjectOrientation/scripts/ObjectOrientation.lua
+++ ObjectOrientation/scripts/ObjectOrientation.lua
@@ -1,36 +1,29 @@
-
---Start of Global Scope--------------------------------------------------------- 
-
--- main function, which will be called when the engine.onStarted event has occurred.
-function main()
-  tmr = Timer.create() -- creation of handle object
-  tmr:setExpirationTime(1000)
-  tmr:setPeriodic(true)
-  tmr:register("OnExpired","handleOnExpired")
-   -- This will call the startTimer function as well as print the return string
-  print(startTimer(tmr))
-end
-
-
--- Registration to the engine.onStartedEvent
-Script.register("Engine.OnStarted", main)
-
---End of Global Scope----------------------------------------------------------- 
-
 --Start of Function and Event Scope---------------------------------------------
 
--- The function is given the Timer handle as parameter and it returns a string
--- The following annotation starting with @ is important to use object orientation and 
--- code completion for and within functions
---@startTimer(timerAsParameter:Timer):string
-function startTimer(timerAsParameter)
+---The function is given the Timer handle as parameter and it returns a string
+---@param timerAsParameter Timer
+---@return string
+local function startTimer(timerAsParameter)
   timerAsParameter:start()
   return("Timer started")
 end
 
- function handleOnExpired()
+local function handleOnExpired()
   print("The timer expired")
-end  
+end
+
+---main function, which will be called when the engine.onStarted event has occurred.
+local function main()
+  tmr = Timer.create() -- creation of handle object
+  tmr:setExpirationTime(1000)
+  tmr:setPeriodic(true)
+  tmr:register("OnExpired",handleOnExpired)
+   -- This will call the startTimer function as well as print the return string
+  print(startTimer(tmr))
+end
+
+-- Registration to the engine.onStartedEvent
+Script.register("Engine.OnStarted", main)
 
 --End of Function and Event Scope------------------------------------------------
 
